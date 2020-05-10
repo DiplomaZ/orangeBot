6eeb6eff9ca489a3f0d4a802333a87b928a76f52
Victor <victor.tran@tempestas.io>
2020-05-10 03:15:02 -0400

    add a way to extract the command a user enters

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,11 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (msg) => {
-    if (msg.content === "What's your name?") {
-        msg.reply(
-            "I don't really have an official name yet, but one of my creators really likes the color orange.",
-        );
-        // msg.channel.send("pong");
-    } else if (msg.content.startsWith('!kick')) {
-        if (msg.mentions.users.size) {
-            const taggedUser = msg.mentions.users.first();
-            msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-        } else {
-            msg.reply('Please tag a valid user!');
-        }
-    }
+    const getCommand = (message: Message): string | void => {
+        // if first letter isn't prefix -> ! ignore or if message from bot, ignore
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+        const command = message.content.split(' ')[0].replace(prefix, '');
+        return command;
+    };
 });
