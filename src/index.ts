b4cb6d7128f0cbf46a96d56a462210018dd60366
Victor <victor.tran@tempestas.io>
2020-05-10 04:21:57 -0400

    add function to create user

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,26 @@ bot.on('ready', () => {
 });
 
 bot.on('message', (msg) => {
-    if (msg.content === "What's your name?") {
-        msg.reply(
-            "I don't really have an official name yet, but one of my creators really likes the color orange.",
+    const createUser = (user: User): TempUser => {
+        // create user
+        // ! TODO: refactor into class
+        const newUser: TempUser = {
+            id: user.id,
+            balance: 0,
+            name: user.username,
+        };
+
+        // add user to 'db'
+        // ! TODO: implement save to db
+        users.push(newUser);
+
+        user.lastMessage.channel.send(
+            'Profile created due to existing user not having profile',
+        );
+
+        return newUser;
+    };
+
         );
         // msg.channel.send("pong");
     } else if (msg.content.startsWith('!kick')) {
