8f73148b2b91652f591b8c87bcf20ad678af079f
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-05-09 18:53:35 -0700

    changed greeting string

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
-bot.on('ready', () => {
+bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  } else if (msg.content.startsWith('!kick')) {
+bot.on("message", (msg) => {
+  if (msg.content === "What's your name?") {
+    msg.reply(
+      "I don't really have an official name yet, but one of my creators really likes the color orange."
+    );
+    // msg.channel.send("pong");
+  } else if (msg.content.startsWith("!kick")) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
       msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
     } else {
-      msg.reply('Please tag a valid user!');
+      msg.reply("Please tag a valid user!");
     }
   }
 });
