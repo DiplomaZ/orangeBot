6ed845e7ce2f9a428629957d573f7aab4e23e72c
Victor <victor.tran@tempestas.io>
2020-05-10 20:40:34 -0400

    curried function to have access to Client

--- src/core/onMessageHandler.ts
+++ src/core/onMessageHandler.ts
@@ -1,8 +1,6 @@
-import { prefix } from '../../discord.config.json';
-import { Message } from 'discord.js';
+import { Message, Client } from 'discord.js'
 
-const onMessageHandler = (msg: Message): void => {
-    const getCommand = (message: Message): string | void => {
+const onMessageHandler = (client: Client) => (message: Message): void => {
         // if first letter isn't prefix -> ! ignore or if message from bot, ignore
         if (!message.content.startsWith(prefix) || message.author.bot) return;
 
