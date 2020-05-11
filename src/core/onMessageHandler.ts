a4663282330560ce9784e7015dd9f5b68e7cfa26
Victor <victor.tran@tempestas.io>
2020-05-10 21:53:51 -0400

    add semi colons

--- src/core/onMessageHandler.ts
+++ src/core/onMessageHandler.ts
@@ -1,25 +1,25 @@
-import { prefix } from '../../discord.config.json'
-import { Message, Client } from 'discord.js'
+import { prefix } from '../../discord.config.json';
+import { Message, Client } from 'discord.js';
 
 const onMessageHandler = (client: Client) => (message: Message): void => {
     // if first letter isn't prefix -> ! ignore or if message from bot, ignore
-    if (!message.content.startsWith(prefix) || message.author.bot) return
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/)
-    const command = args.shift().toLowerCase()
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
     // @ts-ignore
-    if (!client.commands.has(command)) return
+    if (!client.commands.has(command)) return;
 
     try {
         // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
         // @ts-ignore
-        client.commands.get(command).execute(message, args)
+        client.commands.get(command).execute(message, args);
     } catch (error) {
-        console.error(error)
-        message.reply('there was an error trying to execute that command!')
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
     }
-}
+};
 
-export default onMessageHandler
+export default onMessageHandler;
--- src/model/user.ts
+++ src/model/user.ts
@@ -1,41 +1,45 @@
+import moment from 'moment';
 interface BasicStats {
-    intelligence: number
-    agility: number
-    strength: number
+    intelligence: number;
+    agility: number;
+    strength: number;
 }
 
 interface DamageStats {
-    damage: number
+    damage: number;
 }
 
 interface ArmorStats {
-    hitPoints: number
-    armor: number
+    hitPoints: number;
+    armor: number;
 }
 
 interface UserStats extends BasicStats, DamageStats, ArmorStats {
-    level: number
+    level: number;
 }
 
 interface UserConfig {
-    name: string
-    balance: number
-    hitPoints: number
-    level: number
-    strength: number
-    agility: number
-    intelligence: number
-    damage: number
-    armor: number
+    id: string;
+    name: string;
+    balance?: number;
+    hitPoints?: number;
+    level?: number;
+    strength?: number;
+    agility?: number;
+    intelligence?: number;
+    damage?: number;
+    armor?: number;
 }
 
 class User {
-    private name: string
-    private balance: number
-    private stats: UserStats
+    private id: string;
+    private name: string;
+    private balance: number;
+    private stats: UserStats;
 
     constructor(config: UserConfig) {
         const {
+            id,
             name = '',
             balance = 0,
             hitPoints = 0,
@@ -45,7 +49,7 @@ class User {
             intelligence = 1,
             damage = 10,
             armor = 10,
-        } = config
+        } = config;
 
         const stats = {
             hitPoints,
@@ -55,33 +59,41 @@ class User {
             intelligence,
             damage,
             armor,
-        }
-        this.name = name
-        this.balance = balance
-        this.stats = stats
+        };
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.stats = stats;
+    }
+
+    public getId(): string {
+        return this.id;
     }
 
     public getName(): string {
-        return this.name
+        return this.name;
     }
 
     public getBalance(): number {
-        return this.balance
+        return this.balance;
     }
 
     public getStats(): UserStats {
-        return this.stats
+        return this.stats;
     }
 
     public setName(name: string): void {
-        this.name = name
+        this.name = name;
     }
 
     public checkIn(): void {
+        this.balance = this.balance + 200;
+    }
+
         this.balance = this.balance + 200
     }
 
     // ! TODO: figure out more methods needed
 }
 
-export default User
+export default User;
