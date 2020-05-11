175c9a7a7a8807b6278e0d5b8c3f6a1794ac8482
Victor <victor.tran@tempestas.io>
2020-05-10 20:42:47 -0400

    create a way to execute command

--- src/core/onMessageHandler.ts
+++ src/core/onMessageHandler.ts
@@ -1,7 +1,8 @@
+import { prefix } from '../../discord.config.json'
 import { Message, Client } from 'discord.js'
 
 const onMessageHandler = (client: Client) => (message: Message): void => {
-        // if first letter isn't prefix -> ! ignore or if message from bot, ignore
+    // if first letter isn't prefix -> ! ignore or if message from bot, ignore
     if (!message.content.startsWith(prefix) || message.author.bot) return
 
     const args = message.content.slice(prefix.length).split(/ +/)
@@ -11,3 +12,14 @@ const onMessageHandler = (client: Client) => (message: Message): void => {
     // @ts-ignore
     if (!client.commands.has(command)) return
 
+    try {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        client.commands.get(command).execute(message, args)
+    } catch (error) {
+        console.error(error)
+        message.reply('there was an error trying to execute that command!')
+    }
+}
+
+export default onMessageHandler
