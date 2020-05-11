2b55d13539a9823172152e202babca07ad445fb0
Victor <victor.tran@tempestas.io>
2020-05-10 20:41:18 -0400

    extract args/commands

--- src/core/onMessageHandler.ts
+++ src/core/onMessageHandler.ts
@@ -2,11 +2,13 @@ import { Message, Client } from 'discord.js'
 
 const onMessageHandler = (client: Client) => (message: Message): void => {
         // if first letter isn't prefix -> ! ignore or if message from bot, ignore
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return
+
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
 
         const command = message.content.split(' ')[0].replace(prefix, '');
         return command;
     };
 };
 
-export default onMessageHandler;
