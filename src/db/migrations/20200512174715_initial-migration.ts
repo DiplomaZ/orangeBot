d3858f9ac1af81b782f0ef2b68931980080e588c
Victor <victor.tran@tempestas.io>
2020-05-13 01:17:29 -0400

    add user migration

---
+++ src/db/migrations/20200512174715_initial-migration.ts
@@ -0,0 +1,16 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+    return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
+        table.increments('id');
+        table.string('discordID').notNullable().unique();
+        table.float('balance').defaultTo(0);
+        table
+            .string('lastCheckIn')
+            .defaultTo((Date.now() - 1000 * 60 * 60 * 24).toString());
+    });
+}
+
+export async function down(knex: Knex): Promise<any> {
+    return knex.schema.dropTableIfExists('users');
+}
