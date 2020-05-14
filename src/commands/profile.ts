8a04fb33f90a0da09d42b13d72a1ea84747a6991
Victor <victor.tran@tempestas.io>
2020-05-13 22:01:57 -0400

    create a progress bar in text

--- src/commands/profile.ts
+++ src/commands/profile.ts
@@ -6,6 +6,25 @@ module.exports = {
     description: '',
     execute(message: Message, arg): void {
         loadProfile(message, user => {
+            const [level, experience, expToNextLevel] = user.level;
+            // ! refactor by moving function into helper folder or make a user method
+            const createProgressBar = (
+                experience,
+                expToNextLevel,
+                chunks = 30
+            ): string => {
+                const progressPercentage = experience / expToNextLevel;
+                const progressInChunks = Math.round(
+                    progressPercentage * chunks
+                );
+                const filledBar = '■';
+                const unfilledBar = '□';
+                const progressBar = filledBar
+                    .repeat(progressInChunks)
+                    .replace(/\s/g, '')
+                    .concat(unfilledBar.repeat(chunks - progressInChunks));
+                return progressBar;
+            };
             message.channel.send(
                 `Hello, ${username}, how are you today? Your account balance is ${user.balance}.`
             );
