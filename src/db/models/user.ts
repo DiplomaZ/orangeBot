32b1e67dc1cb88ac102e6457f50bf4d36e1a86ac
victor tran <victor.tran@tempestas.io>
2020-05-15 15:58:01 -0400

    modify find method

--- src/db/models/user.ts
+++ src/db/models/user.ts
@@ -96,7 +96,7 @@ class User {
 
     static find(): Promise<UserConfig[]> {
         // returns all users
-        return User.database;
+        return User.database.select('*');
     }
 
     static findByDiscord(
@@ -111,7 +111,7 @@ class User {
             return User.database.where({ discordID: value.value }).select('*');
         }
     }
-
+    //! Del: system32
     static findById(id: number): Promise<UserConfig> {
         return User.database.where({ id }).select('*');
     }
